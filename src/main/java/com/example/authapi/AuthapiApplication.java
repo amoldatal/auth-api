14517fca9a9bc0e76e68da04a0766b26b58c3f94
amoldtal <amoldatal21@gmail.com>
2025-05-31 10:54:53 +0900

    Fix: Correct mappings and method issues

--- src/main/java/com/example/authapi/AuthapiApplication.java
+++ src/main/java/com/example/authapi/AuthapiApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AuthApiApplication {
+public class AuthapiApplication {
     public static void main(String[] args) {
-        SpringApplication.run(AuthApiApplication.class, args);
+        SpringApplication.run(AuthapiApplication.class, args);
     }
 }
 
